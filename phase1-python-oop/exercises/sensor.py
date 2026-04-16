0efc7b7c23c0197499ba742bd1e0ed200412f4fe
guilhermegma <guilhermegmlandrade@gmail.com>
2026-04-16 14:31:48 -0300

    feat: create Sensor class with add_value and print_list methods

---
+++ phase1-python-oop/exercises/sensor.py
@@ -0,0 +1,29 @@
+class Sensor:
+    def __init__(self, name, unitOfMeasurement, frequencies):
+        self.name = name
+        self.unitOfMeasurement = unitOfMeasurement
+        self.frequencies = frequencies
+        self.readings = []
+
+    def add_value (self, value):
+        self.readings.append (value)
+
+    def print_list (self):
+        print(f"Reading values are: {self.readings}")
+
+def main():
+    
+    Sensor1 = Sensor('RadioFrequency', 'GHz', 24)
+    Sensor2 = Sensor('Radar', 'GHz', 300)
+
+    Sensor1.add_value(32)
+    Sensor1.add_value(56)
+    Sensor1.add_value(78)
+
+    Sensor2.add_value(90)
+
+    Sensor1.print_list()
+    Sensor2.print_list()
+
+if __name__ == '__main__':
+    main()
